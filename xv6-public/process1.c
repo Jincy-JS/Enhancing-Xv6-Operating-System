5f899999a24e27600604d7d68a57e684ba34ed64
Jincy-JS <jincyjs2003.com>
2023-08-18 00:11:13 +0530

    message

---
+++ xv6-public/process1.c
@@ -0,0 +1,19 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+
+    int i;
+    int prod = 5;
+    int n = 999;
+    for(i=0; i<n; i++){
+        prod = prod * n;
+    }
+    printf(1,"1 - product is : %d\n", prod);
+
+    exit();
+}
+
